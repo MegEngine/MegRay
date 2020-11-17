960c574261f03beb054b7bb3694c2076b6558d2f
Megvii Engine Team <megengine@megvii.com>
2020-11-17 11:22:11 +0800

    build(rccl): detect hip version and link correct host lib
    GitOrigin-RevId: 8081bc890abfb5050664000217d61283a9fd5082

--- cmake/rccl.cmake
+++ cmake/rccl.cmake
@@ -10,7 +10,7 @@ endif()
 
 add_custom_command(
     OUTPUT ${RCCL_LIBS}
-    COMMAND CXX=${HIP_HIPCC_EXECUTABLE} cmake -S ${RCCL_DIR} -B ${RCCL_BUILD_DIR}
+    COMMAND CXX=${HIP_HIPCC_EXECUTABLE} cmake -S ${RCCL_DIR} -B ${RCCL_BUILD_DIR} -DCMAKE_CXX_COMPILER_FORCED=ON
     COMMAND ${MAKE_COMMAND} -C ${RCCL_BUILD_DIR}
     WORKING_DIRECTORY ${RCCL_DIR}
     VERBATIM)
@@ -24,8 +24,8 @@ set_target_properties(
     librccl PROPERTIES
     IMPORTED_LOCATION ${RCCL_LIBS}
 )
-target_include_directories(librccl INTERFACE "${RCCL_BUILD_DIR};${HIP_INCLUDE_DIRS}")
+target_include_directories(librccl INTERFACE ${RCCL_BUILD_DIR} ${HIP_INCLUDE_DIRS})
 target_link_directories(librccl INTERFACE ${HIP_LIB_INSTALL_DIR})
-target_link_libraries(librccl INTERFACE ${CMAKE_DL_LIBS} rt numa)
+target_link_libraries(librccl INTERFACE ${CMAKE_DL_LIBS} rt numa ${HIP_LIBRARY})
 target_compile_options(librccl
-    INTERFACE ${HIP_CONFIG_RESULT_LIST})
+    INTERFACE ${HIP_COMPILE_OPTIONS})
